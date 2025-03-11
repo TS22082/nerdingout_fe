f344387729045d64bbf6c260ed5c9c67cb2f75d6
nerdingout <thomas@estatespace.com>
2025-03-10 20:52:14 -0700

    new: implement protected route on new / edit / delete pages

--- src/components/containers/AuthProtected/useAuthProtectedData.ts
+++ src/components/containers/AuthProtected/useAuthProtectedData.ts
@@ -6,7 +6,22 @@ const useAuthProtectedData = () => {
 
   if (!authToken) return navigate('/');
 
-  console.log('auth token ==>', authToken);
+  (async () => {
+    try {
+      const baseUrl = import.meta.env.VITE_API_BASE_URL;
+      const response = await fetch(`${baseUrl}/auth/verify`, {
+        method: 'GET',
+        headers: {
+          Authorization: authToken,
+        },
+      });
+
+      const data = await response.json();
+      if (!data.success) return navigate('/');
+    } catch (err) {
+      console.error('error making request ==>', err);
+    }
+  })();
 };
 
 export default useAuthProtectedData;
